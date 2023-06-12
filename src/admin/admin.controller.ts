36cda8af0178f269a715256ceae5d9321ef36dcd
Hyunjoong <de0978@gmail.com>
2023-06-12 15:21:11 +0900

    feat: Add get sign mode

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,24 +1,19 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { AdminService } from './admin.service';
-import { CreateAdminDto } from './dto/create-admin.dto';
 import { Role } from 'src/common/enums/role.enum';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { SignupMode } from 'src/common/enums/sign-mode.enum';
-import { UpdateAdminDto } from './dto/update-admin.dto';
 
 @Controller('admin')
 @Roles(Role.Admin)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Get('/sign/mode')
+  async getSignupMode() {
+    return { mode: this.adminService.getSignupMode() };
+  }
 
   @Post('/sign/link')
   async updateSignupModeLink() {
